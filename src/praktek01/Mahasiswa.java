bfe9157d45e8ed65b709819d8fd2d5930f8d4e27
rrpenny123 <penni.app123@gmail.com>
2023-05-28 22:43:22 +0800

    Membuat Attribute Behavior

--- src/praktek01/Mahasiswa.java
+++ src/praktek01/Mahasiswa.java
@@ -1,5 +1,13 @@
 package praktek01;
 
 public class Mahasiswa {
+    String nama;
+    String NPM;
+    double IPK;
     
+    void cetakInfo () {
+        System.out.println("Nama :"+nama);
+        System.out.println("NPM :"+NPM);
+        System.out.println("IPK :"+IPK);
+    }
 }
