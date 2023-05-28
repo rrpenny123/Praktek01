c2813e107a6ab389e9630540477b62b364a12d54
rrpenny123 <penni.app123@gmail.com>
2023-05-28 22:44:39 +0800

    Mengisi Nilai Attribute

--- src/praktek01/mahasiswaAksi.java
+++ src/praktek01/mahasiswaAksi.java
@@ -3,5 +3,9 @@ package praktek01;
 public class mahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Harry = new Mahasiswa ();
+        
+        Harry.nama="Harry Potter";
+        Harry.NPM="19639999";
+        Harry.IPK=3.4;
     }
 }
